2e8c4777c5fcead6be43117a026f3b22a31e7578
Work <tmarzan913@gmail.com>
2022-04-17 18:12:33 +0300

    17.04

--- app/src/main/java/com/example/onboardingsense/Activities/PayActivity.kt
+++ app/src/main/java/com/example/onboardingsense/Activities/PayActivity.kt
@@ -1,13 +1,28 @@
 package com.example.onboardingsense.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.onboardingsense.R
+import com.example.onboardingsense.databinding.ActivityMainBinding
+import com.example.onboardingsense.databinding.ActivityPayBinding
 
 class PayActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPayBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pay)
+        binding = ActivityPayBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         supportActionBar?.hide()
+
+        binding.btnTry.setOnClickListener {
+            val startmeditact =
+                Intent(this, ShortMeditationActivity::class.java)
+            startActivity(startmeditact)
+        }
     }
 }
